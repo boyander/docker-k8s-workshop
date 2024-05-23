332d46086146d6730d69df43ef827f218bfc023d
Marc Pomar <boyander@boyander-m3-mbp.local>
2024-05-23 11:35:38 +0200

    fix: samples

--- app-sample/build.sh
+++ app-sample/build.sh
@@ -2,6 +2,6 @@
 
 TAG="v1"
 
-docker buildx build --platform linux/arm64/v8,linux/amd64 -t boyander/app-sample:$TAG .
+docker build -t boyander/app-sample:$TAG .
 docker push boyander/app-sample:$TAG
 kubectl apply -f k8s/pod.yaml
